ad6e88be0ad6ce40f7f651cda81232ba9fc6ed3d
Bohdan Chokhlenko <105508224+den-ffv@users.noreply.github.com>
2025-01-22 00:08:42 +0200

    minor update

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,16 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 
-// Middleware для парсингу JSON
 app.use(express.json());
 
-// Привітальний маршрут
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello from TypeScript + Express!');
+  res.send('Hi from TypeScript + Express!');
+});
+
+// Глобальний обробник помилок
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'There was an error on the server!' });
 });
 
 export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
 import app from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`The server is running on the port ${PORT} 🚀`);
 });
