8db1d75dfb982b0232679d30d087cb45ecc333f9
Bohdan Chokhlenko <105508224+den-ffv@users.noreply.github.com>
2025-01-22 23:21:32 +0200

    add prisma orm

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,6 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hi from TypeScript + Express!');
 });
 
-// Глобальний обробник помилок
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).json({ message: 'There was an error on the server!' });
